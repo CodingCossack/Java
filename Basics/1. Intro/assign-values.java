94da3ba6d92878881a266b19229f225c76a3860b
Dima <108333654+CodingCossack@users.noreply.github.com>
2023-04-04 19:32:18 +0100

    Add files via upload

---
+++ Basics/1. Intro/assign-values.java
@@ -0,0 +1,15 @@
+// Program to assign values to double and char variables.
+
+
+class Main {
+    public static void main(String[] args) {
+
+        // create variables
+        double number = 78.34;
+        char alphabet = 'u';
+
+        // print the double and character variables
+        System.out.println(number);
+        System.out.println(alphabet);
+    }
+}
