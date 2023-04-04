4914759be60d27ab86f2f0d1bc4505ed6f61fc80
Dima <108333654+CodingCossack@users.noreply.github.com>
2023-04-04 19:35:06 +0100

    Add files via upload

---
+++ Basics/1. Intro/explicit-type-conversion.java
@@ -0,0 +1,26 @@
+// Program to add variables of different data types.
+
+
+class Main {
+
+    public static void main(String[] args) {
+
+        double number = 18.7;
+
+        int result;
+
+        // convert number to integer and add 5
+        result = (int) 18.7 + 5;
+        System.out.println(result);
+    }
+}
+
+/*
+
+Type Conversion: Process of converting one data type into another
+
+Implicit Type Conversion: one data type is automatically converted to another type
+
+Explicit type conversion: conversion from one type to another regardless of the loss in data
+
+*/
