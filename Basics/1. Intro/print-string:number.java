543b7624f30416abf7e5275b5acb8986a5a02d02
Dima <108333654+CodingCossack@users.noreply.github.com>
2023-04-04 19:40:24 +0100

    Add files via upload

---
+++ Basics/1. Intro/print-string:number.java
@@ -0,0 +1,9 @@
+// Program to print the string Java is interesting. and the number 67 in two separate lines.
+
+
+class Main {
+    public static void main(String[] args) {
+        System.out.println("Java is interesting.");
+        System.out.println(67);
+    }
+}
