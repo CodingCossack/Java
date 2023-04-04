988941d6a210c35157478ee77c2814cce64c4960
Dima <108333654+CodingCossack@users.noreply.github.com>
2023-04-04 19:40:45 +0100

    Add files via upload

---
+++ Basics/1. Intro/print-variables-double.java
@@ -0,0 +1,15 @@
+// Program to print a variable
+
+
+class Main {
+    public static void main(String[] args) {
+
+        // create two variables
+        int number1 = 89;
+        double number2 = 313.78;
+
+        // print variables
+        System.out.println(number1);
+        System.out.println(number2);
+    }
+}
