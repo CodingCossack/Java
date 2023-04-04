a6750eb6f3ac5d815bf2a7257e881ff591a5371b
Dima <108333654+CodingCossack@users.noreply.github.com>
2023-04-04 19:33:52 +0100

    Add files via upload

---
+++ Basics/1. Intro/changing-variable-numbers.java
@@ -0,0 +1,17 @@
+// Program to change the value of a variable.
+
+
+class Main {
+    public static void main(String[] args) {
+     
+        // create and print the number variable
+        double number = 32.54;
+        System.out.println(number);
+     
+        // change the value of number to 87.62
+        number = 87.62;
+      
+        // print the number variable
+        System.out.println(number);
+     }
+}
