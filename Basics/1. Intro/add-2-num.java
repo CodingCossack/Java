06bae42f3f1fa602f3de31d615b625cdf48b4640
Dima <108333654+CodingCossack@users.noreply.github.com>
2023-04-04 19:31:58 +0100

    Add files via upload

---
+++ Basics/1. Intro/add-2-num.java
@@ -0,0 +1,14 @@
+// Program to perform addition between two numbers.
+
+class Main {
+    public static void main(String[] argos) {
+
+        // create two variables
+        int number1 = 2;
+        int number2 = 3;
+
+        // add two variables
+        int sum = number1 + number2;
+        System.out.println(sum);
+    }
+}
