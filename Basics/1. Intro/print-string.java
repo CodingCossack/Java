b5ff26782fbee7806e47f6af591b5b1f24bb17b6
Dima <108333654+CodingCossack@users.noreply.github.com>
2023-04-04 19:40:07 +0100

    Add files via upload

---
+++ Basics/1. Intro/print-string.java
@@ -0,0 +1,9 @@
+// Program to print the sentence I love Java Programming. on the screen.
+
+
+class Main {
+    public static void main(String[] args) {
+        // print the sentence
+        System.out.println("I love Java Programming.");
+    }
+}
