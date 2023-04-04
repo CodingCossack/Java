2e05cee2d82729f454859d393955064a46926e33
Dima <108333654+CodingCossack@users.noreply.github.com>
2023-04-04 19:39:23 +0100

    Add files via upload

---
+++ Basics/1. Intro/java-print-text.java
@@ -0,0 +1,9 @@
+// Program to print Java Programming.
+
+
+class Main {
+    public static void main(String[] args) {
+        // print Java Programming
+        System.out.println("Java" + " Programming");
+    }
+}
