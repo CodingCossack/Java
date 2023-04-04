68d098a9a361c9c99c0ecebfbb6789462a452625
Dima <108333654+CodingCossack@users.noreply.github.com>
2023-04-04 19:39:43 +0100

    Add files via upload

---
+++ Basics/1. Intro/minus-operator.java
@@ -0,0 +1,13 @@
+// Program to calculate the profit and loss based on the cost price and selling price.
+
+class Main {
+    public static void main(String[] args) {
+
+        int costPrice = 129;
+        int sellingPrice = 191;
+
+        // compute the profit and print it
+        int profit = sellingPrice - costPrice;
+        System.out.println(profit);
+    }
+}
