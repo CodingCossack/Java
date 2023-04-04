4c125cf07b50c17602f97f124633c6106e864b9e
Dima <108333654+CodingCossack@users.noreply.github.com>
2023-04-04 19:42:09 +0100

    Add files via upload

---
+++ Basics/1. Intro/type-conversion.java
@@ -0,0 +1,24 @@
+// Program to add double and int variables.
+
+class Main {
+    public static void main(String[] args) {
+     
+        // create an integer variable
+        int number1 = 34;
+     
+        // create a double variable
+        double number2 = 54.9;
+     
+        // add int and double variables and print the result
+        double result = number1 + number2;
+        System.out.println(result);
+    }
+}
+
+/*
+
+Type Conversion: Process of converting one data type into another
+
+Implicit Type Conversion: one data type is automatically converted to another type
+
+*/
