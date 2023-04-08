e6b82866b4e2eafcfb9f6cd604b03e32c35c4b35
Dima <108333654+CodingCossack@users.noreply.github.com>
2023-04-08 21:27:53 +0100

    Add files via upload

---
+++ Basics/2. Flow Control/the-while-loop.java
@@ -0,0 +1,16 @@
+// Program to print numbers from 5 to 1 using a while loop.
+
+
+class Main {
+    public static void main(String[] args) {
+
+        // declare variable
+        int i = 5;
+
+        // while loop
+        while (i > 0) {
+            System.out.println(i);
+            i = i - 1;
+        }
+    }
+}
