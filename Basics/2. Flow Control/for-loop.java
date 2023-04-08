9b5c981bdfc0b640786c7577020549571d43f546
Dima <108333654+CodingCossack@users.noreply.github.com>
2023-04-08 21:26:31 +0100

    Add files via upload

---
+++ Basics/2. Flow Control/for-loop.java
@@ -0,0 +1,16 @@
+// Program to print numbers from 5 to 1 using a for loop.
+
+
+class Main {
+
+    public static void main(String[] args) {
+        // run a for loop from i = 5 to 1
+        // print the value of i
+        for (int i = 5; i > 0; i--) {
+            System.out.println(i);
+        }
+    }
+}
+
+
+// for (intitalization; boolean; update expression)
