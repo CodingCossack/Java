f967970cb31beaa43f19d08c486d43c37af9ad87
Dima <108333654+CodingCossack@users.noreply.github.com>
2023-04-22 10:37:06 +0100

    Add files via upload

---
+++ Basics/3. Arrays/java-array.java
@@ -0,0 +1,15 @@
+// Program to implement a double type array.
+
+
+class Main {
+    public static void main(String[] args) {
+        
+        // create double array
+        double[] numbers = {3.5, 8.9, 1.6, -4.8};
+        
+        // access array elements using the for loop
+        for (int i = 0; i < 4; i++) {
+            System.out.println(numbers[i]);
+        }
+    }
+}
