5b7e0111072fab0ebcfcb232ddafbcd00ac31cf6
Dima <108333654+CodingCossack@users.noreply.github.com>
2023-04-24 15:51:31 +0100

    Add files via upload

---
+++ Basics/4. Methods/print-random-number.java
@@ -0,0 +1,14 @@
+// Program to generate random numbers between 1 to 100.
+
+
+class Main {
+    public static void main(String[] args) {
+
+        // random number from 1 to 100
+        int randomNumber = (int) (Math.random() * 100 + 1);
+
+        System.out.println(randomNumber);
+  }
+}
+
+// System.out.println(), nextInt(), nextDouble(), etc are called standard library methods.
