e7009912a16d5b9f8e8f334bccf1ca683490bad9
Dima <108333654+CodingCossack@users.noreply.github.com>
2023-04-24 15:51:17 +0100

    Add files via upload

---
+++ Basics/4. Methods/method.java
@@ -0,0 +1,25 @@
+// Program to print numbers using a method.
+
+
+class Main {
+
+    // create printNumbers method
+    void printNumbers() {
+        // print 5
+        System.out.println(5);
+        // print 100
+        System.out.println(100);
+  }
+
+    public static void main(String[] args) {
+
+        // creating object of Main class
+        Main obj = new Main();
+    
+        // call printNumbers()
+        obj.printNumbers();
+    
+        // call printNumbers() again
+        obj.printNumbers();
+    }
+}
