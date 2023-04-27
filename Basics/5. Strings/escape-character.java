e804ab8a275457744b15347697d98e913e817821
Dima <108333654+CodingCossack@users.noreply.github.com>
2023-04-27 22:09:33 +0100

    Add files via upload

---
+++ Basics/5. Strings/escape-character.java
@@ -0,0 +1,15 @@
+// Program to print a text using escape characters.
+
+
+class Main {
+
+    public static void main(String[] args) {
+    
+        // print the following string
+        // She said, "I love ice cream."
+        System.out.println("She said, \"I love ice cream.\"");
+    }
+}
+
+
+// \" & \n are escape sequences
