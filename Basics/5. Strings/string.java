ff843d81c8d9f25848822a267fa14f708d270465
Dima <108333654+CodingCossack@users.noreply.github.com>
2023-04-28 11:43:43 +0100

    Add files via upload

---
+++ Basics/5. Strings/string.java
@@ -0,0 +1,13 @@
+// Program to create a String and print it.
+
+
+class Main {
+    public static void main(String[] args) {
+    
+        // create string variable
+        String text = "Slash";
+   
+        // print it
+        System.out.println(text);
+    }
+}
