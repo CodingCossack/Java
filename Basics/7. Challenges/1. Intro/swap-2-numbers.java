59146a1ac9015402b711a1296eb3a7df3f3626b1
Dima <108333654+CodingCossack@users.noreply.github.com>
2023-05-20 21:43:42 +0100

    Add files via upload

---
+++ Basics/7. Challenges/1. Intro/swap-2-numbers.java
@@ -0,0 +1,46 @@
+// Program to swap two numbers using a temporary variable.
+
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        // get input values for x and y
+        int x = input.nextInt();
+        int y = input.nextInt();
+
+        // temporary variable
+        int temp;
+
+        // swap two values using temp
+        temp = x;
+        x = y;
+        y = temp;
+
+        // print values after swapping
+        System.out.println(x);
+        System.out.println(y);
+
+        input.close();
+    }
+}
+
+/* In Java, input.close() is used to close an input stream that has been opened in a program.
+ This is important because it ensures that system resources are released and prevents memory leaks
+      or other issues that may arise if input streams are not closed properly.
+ When a file is opened for reading using an input stream,
+      the operating system allocates certain resources for that file,
+     such as memory and file handles. If the input stream is not closed properly,
+      these resources may remain allocated, even after the program has finished executing.
+      This can lead to issues such as a lack of system resources or the inability to open or read other files.
+
+Additionally, some input streams may buffer data in memory in order to improve performance.
+If the input stream is not closed properly, any data that has been buffered may not be written to the file or output stream,
+which can result in incomplete or corrupted data.
+
+To avoid these issues, it is important to always close input streams when they are no longer needed.
+This can be done using the input.close() method,
+which releases any resources associated with the input stream and ensures that any buffered data is written to the file or output stream.
+*/
