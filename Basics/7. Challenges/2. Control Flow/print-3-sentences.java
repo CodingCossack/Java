15d7076643bb140b07cce740609b2248a6deff81
Dima <108333654+CodingCossack@users.noreply.github.com>
2023-05-21 21:52:48 +0100

    Add files via upload

---
+++ Basics/7. Challenges/2. Control Flow/print-3-sentences.java
@@ -0,0 +1,14 @@
+// Program to print a sentence 3 times using a loop.
+
+import java.util.Scanner;
+
+class Main {
+
+    public static void main(String[] args) {
+
+        // run a for loop from i = 0 to i < 3
+        for (int i = 0; i < 3; i++) {
+            System.out.println("Java Programming is fun.");
+        }
+    }
+}
