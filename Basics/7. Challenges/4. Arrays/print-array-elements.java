c641962bac0966d06fdab12bb71dae7d53401e0d
Dima <108333654+CodingCossack@users.noreply.github.com>
2023-05-24 23:28:56 +0100

    Add files via upload

---
+++ Basics/7. Challenges/4. Arrays/print-array-elements.java
@@ -0,0 +1,14 @@
+// Program to print individual elements of array.
+
+class Main {
+    public static void main(String[] args) {
+
+        // create an array with elements 1, 2, 3, 4, 5
+        int array[] = {1, 2, 3, 4, 5};
+
+        // loop through the array to print each elements
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+        }
+    }
+}
